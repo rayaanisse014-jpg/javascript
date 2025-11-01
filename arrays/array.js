538ff39bd55d0a5887e33b7a7d15ada6208c8320
Rayaan Abdullahi <rayaanabdullahi@Rayaans-MacBook-Pro.local>
2025-11-01 13:39:18 +1000

    i have studied arrays and string

---
+++ arrays/array.js
@@ -0,0 +1,11 @@
+let shoppingList = ["onion" , "mango" , "banana" ,"mik" , "sugar"]
+// shoppingList.push("Tea")
+// shoppingList.pop()
+// shoppingList.unshift("painkillers")
+// shoppingList.shift()
+console.log(shoppingList.length)
+
+// let age = [20 ,40 , 35 ,60]
+// firstname[2] = "john"
+
+// console.log(age)
--- strings/strings.js
+++ strings/strings.js
@@ -1,8 +1,27 @@
 // let Fullname = "Rayaan Isse"
 //  let fullName = 'Rayaan Isse'
 // let fullname = new String('Rayaan Isse')
-let FirstName = 'Rayaan'
-let lastName = 'Isse'
-let fullname =  `my fullname is ${FirstName} ${lastName} !`
-// console.log(fullname.toString())
-console.log(fullname)
+// let FirstName = 'Rayaan'
+// let lastname = 'Isse' 
+// // console.log(FirstName.indexOf("n"))
+// let fullName = FirstName + "  " + lastname
+
+// console.log(fullName)
+// console.log(FirstName.charAt(4))
+// FirstName = FirstName.trim()
+// console.log(FirstName.length)
+// let lastName = 'Isse'
+// let fullname =  `my fullname is ${FirstName} ${lastName} !`
+// // console.log(fullname.toString())
+// // console.log(fullname)
+
+
+// fullname = fullname.replace("my" , "our")
+// fullname = fullname.toUpperCase()
+
+
+// console.log(fullname)
+
+let paragraph = " OH \t I like Burco \n"
+// let firstPart = paragraph.substring(6,12)
+console.log(paragraph.repeat(20))
