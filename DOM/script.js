fd386dd0c87ac06a1d4627d7c4f05e3d687c9b95
Rayaan Abdullahi <rayaanabdullahi@Rayaans-MacBook-Pro.local>
2025-11-04 13:35:09 +1000

    i studied DOM and objects

--- DOM/script.js
+++ DOM/script.js
@@ -1,7 +1,16 @@
 
 for (let i = 1; i <= 500; i++ ){
 let firstH1 = document.createElement("h1")
-firstH1.textContent = i + "Hello World!"
+firstH1.innerHTML = i + "Hello World!"
+firstH1.style.color = 'red'
 document.body.appendChild(firstH1)
+// document.body.removeChild(firstH1)
 }
+
+let alltheFirstWannaBe = document.querySelectorAll("#wanna-first")
+alltheFirstWannaBe[0].style.color = "red"
+alltheFirstWannaBe[0].style.fontSize = "20px"
+alltheFirstWannaBe[1].style.color = "gold"
+alltheFirstWannaBe[1].style.fontSize = "40px"
+ 
 
---
+++ objects/object.js
@@ -0,0 +1,44 @@
+// let person = {
+//     name: "john Doe",
+//     age: 30,
+//     city: "New York",
+//     saySalaam: function(){
+//         console.log("Asalaamu Alaykum")
+//     }
+// }
+// person.saySalaam()
+// person.age = 50 
+// person.name = "ali jama"
+
+// console.log(person.name)
+// console.log(person["name"])
+//  let {city} = person
+//  console.log(city)
+
+ 
+let vehical = {
+    brand: "Toyato",
+    model: "Camry",
+    year: 2020,
+    color: "red",
+    // owner: person,
+    stop: function() {
+        console.log("The car has stoped")
+    }
+
+}
+console.log(vehical.stop())
+// console.log(person.age)
+// console.log(vehical.owner.name)
+
+
+// let animal = new Object();
+// animal.type = "Dog";
+// animal.breed = "golder retriaver";
+// console.log(animal)
+
+// let animal = {}
+// animal.type = "Dog";
+// // animal.breed = "golder retriaver";
+// console.log(animal)
+
